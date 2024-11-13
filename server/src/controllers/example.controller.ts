8993fa673d6bcdb3663af38780de1fcfc11851fe
Chinz266 <chinkung38@gmail.com>
2024-11-13 12:13:02 +0700

    Tinner Edit

---
+++ server/src/controllers/example.controller.ts
@@ -0,0 +1,27 @@
+import { describe } from "bun:test"
+import Elysia, { t } from "elysia"
+
+export const example = new Elysia()
+    .get("/", () => "Hello World", {
+        detail: {
+            tags: ["Example"],
+            summary: ("Ronaldo"),
+            description: ('Suiiii!!')
+        }
+    })
+    .post("/about", ({ body }) => {
+        return {
+            id: '111',
+            msg: 'hello' + body.name
+        }
+    }, {
+        body: t.Object({
+            name: t.String()
+        }),
+        detail: {
+            tags: ["Example"],
+            summary: ("Messi"),
+            description: ('Not_Suiiii!!')
+        }
+
+    })
