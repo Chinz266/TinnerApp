87c2deffda98e0eb5a1fd17aaf5bca1195f70be5
Chinz266 <chinkung38@gmail.com>
2024-12-24 09:14:51 +0700

    newEdit

--- server/src/services/like.service.ts
+++ server/src/services/like.service.ts
@@ -1,18 +1,18 @@
-import mongoose, { Query } from 'mongoose';
-import { User } from '../models/usre.model';
-import { user, userPagination, userPaginator } from './../types/user.type';
-import { QueryHelper } from '../helper/query.helper';
+import mongoose from "mongoose"
+import { QueryHelper } from "../helper/query.helper"
+import { userPagination, userPaginator, user } from "../types/user.type"
+import { User } from "../models/usre.model"
 
 export const LikeService = {
     toggleLike: async function (user_id: string, target_id: string): Promise<boolean> {
         const target = await User.findById(target_id).select("_id").exec()
         if (!target)
-            throw new Error("Invali target_id")
+            throw new Error("Invalid target_id")
+
         const likeTarget = await User.findOne({
             _id: new mongoose.Types.ObjectId(user_id),
-            following: { $elemMath: { $eq: target._id } }
+            following: { $elemMatch: { $eq: target._id } }
         }).exec()
-
         if (likeTarget) {
             await User.findByIdAndUpdate(user_id, { $pull: { following: target_id } })
             await User.findByIdAndUpdate(target_id, { $pull: { followers: user_id } })
@@ -22,54 +22,62 @@ export const LikeService = {
         }
         return true
     },
-    getFolloers: async function (user_id: string, pagination: userPagination): Promise<userPaginator> {
-        const _query =  User.findById(user_id)
+
+    getFollowers: async function (user_id: string, pagination: userPagination): Promise<userPaginator> {
+        const _query = User.findById(user_id)
             .populate({
                 path: "followers",
                 match: { $and: QueryHelper.parseUserQuery(pagination) },
-                select: '_id username display_name photo introduction interest location gender data_of_birth',
-                populate: { path: "photos" }
+                select: '_id username display_name photos introduction interest location gender date_of_birth',
+                populate: { path: "photos", }
             })
+
         const [docs, total] = await Promise.all([
             _query.exec(),
             User.aggregate([
                 { $match: { _id: new mongoose.Types.ObjectId(user_id) } },
-                { $project: { count: { $size: { $ifNull: ["$followers", []] } } } }
+                { $project: { count: { $size: { $isNull: ["$followers", []] } } } }
             ])
         ])
-        pagination.length =  total[0].count
-        let followers: user[]= []
+        pagination.length = total[0].count
+        let follower: user[] = []
         if (docs) {
-            followers = docs.followers as user[]
+            follower = docs.followers as user[]
         }
         return {
             pagination: pagination,
-            items: followers
+            items: follower
+        }
+
+    },
+
+    getFollowing: async function (user_id: string, pagination: userPagination): Promise<userPaginator> {
+        const _query = User.findById(user_id)
+            .populate({
+                path: "following",
+                match: { $and: QueryHelper.parseUserQuery(pagination) },
+                select: '_id username display_name photos introduction interest location gender date_of_birth',
+                populate: { path: "photos", }
+            })
+
+        const [docs, total] = await Promise.all([
+            _query.exec(),
+            User.aggregate([
+                { $match: { _id: new mongoose.Types.ObjectId(user_id) } },
+                { $project: { count: { $size: { $isNull: ["$following", []] } } } }
+            ])
+        ])
+        pagination.length = total[0].count
+        let following: user[] = []
+        if (docs) {
+            following = docs.followers as user[]
+        }
+        return {
+            pagination: pagination,
+            items: following
         }
     },
-    getFollowing: async function (user_id: string, pagination: userPagination): Promise<userPaginator> {
-        const _query =  User.findById(user_id)
-        .populate({
-            path: "followings",
-            match: { $and: QueryHelper.parseUserQuery(pagination) },
-            select: '_id username display_name photo introduction interest location gender data_of_birth',
-            populate: { path: "photos" }
-        })
-    const [docs, total] = await Promise.all([
-        _query.exec(),
-        User.aggregate([
-            { $match: { _id: new mongoose.Types.ObjectId(user_id) } },
-            { $project: { count: { $size: { $ifNull: ["following", []] } } } }
-        ])
-    ])
-    pagination.length =  total[0].count
-    let following: user[]= []
-    if (docs) {
-        following = docs.following as user[]
-    }
-    return {
-        pagination: pagination,
-        items: following,
-    }
-    },
+
+
+
 }
